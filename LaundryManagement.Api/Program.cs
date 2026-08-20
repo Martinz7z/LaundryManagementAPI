2f4034094c403d5d85d3fbf65315462af45ed089
Martin Zachariasz <Martin.zachariasz@gmail.com>
2026-08-20 21:07:16 +0100

    Configure API for Azure deployment

--- LaundryManagement.Api/Program.cs
+++ LaundryManagement.Api/Program.cs
@@ -49,6 +49,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<LaundryDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
